ed4644dbbed867ea5167bc96120540076b8e247e
aucuparia <aucuparia@users.noreply.github.com>
2019-02-07 14:03:47 +0100

    Add dummy routes/controllers/views files

---
+++ controllers/Datarequest.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Datarequest extends MY_Controller
+{
+#    public function __construct() {}
+    public function index() {
+        loadView('index');
+    }
+}
