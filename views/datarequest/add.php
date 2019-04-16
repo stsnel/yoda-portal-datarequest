d3056d17c445a105463750c1e030e7fee302a21b
aucuparia <aucuparia@users.noreply.github.com>
2019-04-16 14:27:08 +0200

    Add data request submission form view

---
+++ views/datarequest/add.php
@@ -0,0 +1,26 @@
+    <div class="row metadata-form">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading clearfix">
+                    <h3 class="panel-title pull-left">
+                        Data request submission form
+                    </h3>
+                    <div class="input-group-sm has-feedback pull-right">
+                        <a class="btn btn-default" href="/datarequest">Back</a>
+                    </div>
+                </div>
+                <div class="panel-body">
+                        <div id="form" class="metadata-form"
+                             data-proposal_id="<?php echo rawurlencode($proposalId); ?>"
+                             data-csrf_token_name="<?php echo rawurlencode($tokenName); ?>"
+                             data-csrf_token_hash="<?php echo rawurlencode($tokenHash); ?>">
+                            <p>Loading metadata <i class="fa fa-spinner fa-spin fa-fw"></i></p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script type="text/javascript">
+</script>
+<script src="/datarequest/static/js/datarequest/add.js" type="text/javascript"></script>
