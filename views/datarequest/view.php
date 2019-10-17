3dd03fa241d949509da4f73223466bcb4fb731dc
aucuparia <aucuparia@users.noreply.github.com>
2019-10-17 15:26:26 +0200

    Add link to data manager review form on detail page

--- views/datarequest/view.php
+++ views/datarequest/view.php
@@ -83,6 +83,9 @@
 <?php if ($requestStatus == "assigned" && $isReviewer): ?>
     <a href="/datarequest/review/<?php echo html_escape($requestId) ?>" class="btn btn-default pull-right" role="button">Review data request</a>
 <?php endif ?>
+<?php if ($requestStatus == "accepted_for_review" && $isDatamanager): ?>
+    <a href="/datarequest/datamanagerreview/<?php echo html_escape($requestId) ?>" class="btn btn-default pull-right" role="button">Data manager review</a>
+<?php endif ?>
 <?php if ($requestStatus == "submitted" && $isBoardMember): ?>
     <a href="/datarequest/preliminaryreview/<?php echo html_escape($requestId) ?>" class="btn btn-default pull-right" role="button">Preliminary review</a>
 <?php endif ?>
