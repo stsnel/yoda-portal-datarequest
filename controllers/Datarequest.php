854e5f82568daf10bbc6e3bd49ea3464fd26a67c
aucuparia <aucuparia@users.noreply.github.com>
2020-11-24 13:05:39 +0100

    Add missing permission check

--- controllers/Datarequest.php
+++ controllers/Datarequest.php
@@ -53,7 +53,7 @@ class Datarequest extends MY_Controller
         $isReviewer     = $this->api->call('datarequest_is_reviewer', ['request_id' => $requestId]);
 
         # If the user is neither of the above, return a 403
-        if (!$isBoardMember && !$isDatamanager && !$isDMCMember && !$isRequestOwner) {
+        if (!$isBoardMember && !$isDatamanager && !$isDMCMember && !$isRequestOwner && !$isReviewer) {
             $this->output->set_status_header('403');
             return;
         }
